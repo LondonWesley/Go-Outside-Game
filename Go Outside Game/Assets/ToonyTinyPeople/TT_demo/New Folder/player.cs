2fa20b61defcea8b81230f23caa423af199c3591
NailahNolley <nailah.nolley@gmail.com>
2019-11-01 17:05:25 -0400

    adds animations for idle and walking

--- Go Outside Game/Assets/ToonyTinyPeople/TT_demo/New Folder/player.cs
+++ Go Outside Game/Assets/ToonyTinyPeople/TT_demo/New Folder/player.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     public float Speed;
+    Animator m_Animator;
+
 
 
     // Update is called once per frame
@@ -12,9 +14,20 @@ public class player : MonoBehaviour
     {
         playermovement();
     }
+
+    private void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+    }
     void playermovement() {
         float hor = Input.GetAxis("Horizontal");
         float ver = Input.GetAxis("Vertical");
+
+        bool hasHorizontalInput = !Mathf.Approximately(hor, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(ver, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool("IsWalking", isWalking);
+
         Vector3 playerMovement = new Vector3(hor, 0f, ver) * Speed * Time.deltaTime;
         transform.Translate(playerMovement, Space.Self);
 
