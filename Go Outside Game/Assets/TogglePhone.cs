56e52d467b6e5d17ba656019119a300a9820157b
Wesley London <wesleylondonwl@gmail.com>
2019-11-03 11:42:38 -0500

    Created Some UI functionality for phone
    The players todo list phone now displays on screen. Use q to toggle it on or off.

---
+++ Go Outside Game/Assets/TogglePhone.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TogglePhone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool phoneIsUp = false;
+    public GameObject IphoneUI;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (phoneIsUp)
+            {
+                IphoneUI.SetActive(false);
+                phoneIsUp = false;
+            }   else {
+                IphoneUI.SetActive(true);
+                phoneIsUp = true;
+            }     
+        }
+    }
+}
