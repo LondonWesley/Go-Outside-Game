6370a820b3f294231077223a47a6ab42dada03ac
Jeffrey Hernandez <[jjhernandezvillarreal@aggies.ncat.edu]>
2019-11-03 19:50:34 -0500

    Added a task randomizer that will spawn objects(tasks) that will appear in random locations. Still trying to work on making the objects appear and disappear during a certain time.

---
+++ Go Outside Game/Assets/RandomTasks.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomTasks : MonoBehaviour
+{
+    public GameObject[] Tasks;
+    public Vector3 spawnValues;
+    public float spawnWait;
+    public float spawnMostWait;
+    public float spawnLeastWait;
+    public int startWait;
+    public bool bye;
+
+    int randTask;
+
+    void Start()
+    {
+        StartCoroutine(waitTask());
+    }
+
+    void Update()
+    {
+        spawnWait = Random.Range(spawnLeastWait, spawnMostWait);
+    }
+
+    IEnumerator waitTask()
+    {
+        yield return new WaitForSeconds(startWait);
+        randTask = Random.Range(0, 1);
+
+        Vector3 spawnPosition = new Vector3(Random.Range (-spawnValues.x, spawnValues.x), 1, Random.Range(-spawnValues.z, spawnValues.z));
+        Instantiate(Tasks[randTask], spawnPosition + transform.TransformPoint(0,0,0), gameObject.transform.rotation);
+        if()
+        {
+            bye = false;
+            ToggleVisibility();
+        }
+        yield return new WaitForSeconds(spawnWait);
+    }
+
+    public void ToggleVisibility()
+    {
+        Renderer rend = gameObject.GetComponent<Renderer>();
+
+        if(bye)
+        {
+            rend.enabled = false;
+        } else
+        {
+            rend.enabled = true;
+        }
+    }
+}
