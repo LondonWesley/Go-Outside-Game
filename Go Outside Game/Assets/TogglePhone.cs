16714a13230ce1df06ff074e145c0391475bfbe7
Wesley London <wesleylondonwl@gmail.com>
2019-11-05 05:17:16 -0500

    added running, task list for phone, stamina bar

--- Go Outside Game/Assets/TogglePhone.cs
+++ Go Outside Game/Assets/TogglePhone.cs
@@ -7,9 +7,21 @@ public class TogglePhone : MonoBehaviour
     // Start is called before the first frame update
     public bool phoneIsUp = false;
     public GameObject IphoneUI;
-
-
+    public List<GameObject> listOfTasks;
+    public int[] time;
+    public int taskCount = 0;
+    public int currentTask = 0;
     // Update is called once per frame
+
+    public void addTask(int timez)
+    {
+        if(taskCount < 9)
+        {
+            listOfTasks[taskCount].SetActive(true);
+            taskCount++;
+
+        }
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Q))
--- Go Outside Game/Assets/ToonyTinyPeople/TT_demo/New Folder/player.cs
+++ Go Outside Game/Assets/ToonyTinyPeople/TT_demo/New Folder/player.cs
@@ -1,18 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class player : MonoBehaviour
 {
     public float Speed;
+    public double stamina = 100;
+    public double recover_rate = 0.2;
+    public Slider stamina_bar;
     Animator m_Animator;
+    public GameObject self;
 
 
 
     // Update is called once per frame
     void Update()
     {
+        stamina_bar.value = (int)(stamina);
         playermovement();
+        if (Input.GetKey(KeyCode.LeftShift) && stamina>=1)
+        {
+            Speed = 20;
+            stamina -= 0.3;
+            // ]Debug.Log(stamina);
+
+        } else {
+            Speed = 5;
+           // Debug.Log(stamina);
+            if (stamina< 100)
+                stamina += recover_rate;
+        }
+
+        if(self.transform.position.y < -200)
+        {
+            self.transform.position = new Vector3(48 ,20,227);
+        }
     }
 
     private void Start()
