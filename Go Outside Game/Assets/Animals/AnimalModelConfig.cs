8db75afafef41dd11dcd6e31286fdc763468e25c
Wesley London <wesleylondonwl@gmail.com>
2019-11-06 18:30:06 -0500

    Created an Animal object with easy to change shape script
    Animal object will be used to make more animals of different kinds.

---
+++ Go Outside Game/Assets/Animals/AnimalModelConfig.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalModelConfig : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject animalModel;
+    public GameObject MeshLoader;
+    void Start()
+    {
+        animalModel.AddComponent<BoxCollider>();
+    }
+
+    public void animalShuffle()
+    {
+        int length = MeshLoader.transform.childCount;
+        int selection = Random.Range(1,length);
+        //Debug.Log(number);
+        MeshFilter appearance = animalModel.GetComponent<MeshFilter>();
+        //Destroy(appearance);
+        
+        MeshFilter newMesh = MeshLoader.transform.GetChild(selection).GetComponent<MeshFilter>();
+        appearance.sharedMesh = newMesh.sharedMesh;
+        
+        adjustCollision();
+    }
+    public void adjustCollision()
+    {
+        BoxCollider collider = animalModel.GetComponent<BoxCollider>();
+        Destroy(collider);
+        animalModel.AddComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int number = Random.Range(1, 50);
+        if(number == 1)
+        {
+            animalShuffle();
+            //Debug.Log("SHUFFLING.");
+        }
+    }
+}
