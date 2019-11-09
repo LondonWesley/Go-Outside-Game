78887577ba268e00f1582a5694639b58c87b8f88
Wesley London <wesleylondonwl@gmail.com>
2019-11-09 16:01:19 -0500

    Animal AI started (follows and yeets player?)

---
+++ Go Outside Game/Assets/GeneralAI.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralAI : MonoBehaviour
+{
+    
+    Transform playerTransform;
+    UnityEngine.AI.NavMeshAgent navMesh;
+    public float checkRate = 0.01f;
+    float nextCheck;
+    void Start()
+    {
+       
+        if (GameObject.FindGameObjectWithTag("Player").activeInHierarchy)
+        {
+   
+            playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+        navMesh = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time> nextCheck)
+        {
+            lookAt(playerTransform);
+            goTo(playerTransform);
+            nextCheck = Time.time + checkRate;
+        }
+    }
+
+    public void lookAt(Transform targetTransform)
+    {
+        navMesh.transform.LookAt(targetTransform);
+    }
+    public void goTo(Transform targetTransform)
+    {
+        navMesh.SetDestination(targetTransform.position);
+    }
+}
