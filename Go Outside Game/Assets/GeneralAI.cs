5ba95eb1188fa21ef37212fdd2939fad3835f694
Wesley London <wesleylondonwl@gmail.com>
2019-11-10 14:25:54 -0500

    Animal Attack and Wandering modes
    Animal AI now lets them wander around.  Attack mode allow animals to fight any entities in range.  If a player happens to be in the range it will automatically prioritize attakcking the player over everything else

--- Go Outside Game/Assets/GeneralAI.cs
+++ Go Outside Game/Assets/GeneralAI.cs
@@ -6,16 +6,23 @@ public class GeneralAI : MonoBehaviour
 {
     
     Transform playerTransform;
+    public Transform targetTransform;
+    RangeDetection detector;
     UnityEngine.AI.NavMeshAgent navMesh;
-    public float checkRate = 0.01f;
+    public float checkRate = 1f;
+    int maxSpeed = 12;
+    //modes : 0 is neutral 1 is flee, 2 is attack
+    public int mode = 0;
     float nextCheck;
     void Start()
     {
-       
+        checkRate = 1f;
+        mode = 1;
+        detector = this.transform.Find("DetectRange").GetComponent<RangeDetection>();
         if (GameObject.FindGameObjectWithTag("Player").activeInHierarchy)
         {
-   
             playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+            targetTransform = playerTransform;
         }
         navMesh = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
     }
@@ -23,20 +30,84 @@ public class GeneralAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if(Time.time> nextCheck)
         {
-            lookAt(playerTransform);
-            goTo(playerTransform);
+            //neutral
+            if (mode == 0)
+            {
+                checkRate = 1f;
+                wander();
+            }
+            else if(mode == 1)
+            {
+                checkRate = 0.0001f;
+                attack();   
+            }
             nextCheck = Time.time + checkRate;
         }
     }
 
-    public void lookAt(Transform targetTransform)
+    public void attack()
     {
-        navMesh.transform.LookAt(targetTransform);
+        GameObject target = this.findClosestTarget();
+        if(target != null)
+        {
+            navMesh.SetDestination(target.transform.position);
+            navMesh.transform.LookAt(target.transform);
+            navMesh.speed = 12;
+        }
+        
     }
-    public void goTo(Transform targetTransform)
+    //uses the objects found by the animal's detector object and finds the closest one
+    //If it finds a player it will always be the closest object regardless.
+    //So animals will join teams to fight the player
+    public GameObject findClosestTarget()
     {
-        navMesh.SetDestination(targetTransform.position);
+        GameObject closestObject = null;
+        List<GameObject> targets = detector.getNearByObjects();
+        if (targets.Count>0)
+        {
+            //assumes the closest object is the first one
+            closestObject = targets[0];
+            float closestDist = Vector3.Distance(closestObject.transform.position, this.transform.position);
+            foreach( GameObject current in targets)
+            {
+                //if it's the player it will always take priority
+                if (current.tag == "Player")
+                    return current;
+                float distance = Vector3.Distance(current.transform.position, this.transform.position);
+                if(distance< closestDist)
+                {
+                    closestObject = current;
+                    closestDist = distance;
+                }
+            }
+        }
+        return closestObject;
     }
+
+
+    public void wander()
+    {
+        int action = Random.Range(1, 10);
+        if(action == 1)
+        {
+            Vector3 selfPos = this.transform.position;
+            Vector3 position = new Vector3(selfPos.x + Random.Range(-100.0f, 100.0f), 2, selfPos.z + Random.Range(-100.0f, 100.0f));
+            navMesh.speed = Random.Range(0,maxSpeed);
+            navMesh.SetDestination(position);
+            navMesh.transform.LookAt(position);
+        }
+        if (action == 2)
+        {
+            Vector3 selfPos = this.transform.position;
+            Vector3 position = new Vector3(selfPos.x ,0, selfPos.y);
+            navMesh.speed = 0;
+            navMesh.SetDestination(position);
+            navMesh.transform.LookAt(playerTransform);
+        }
+    }
+
+
 }
---
+++ Go Outside Game/Assets/RangeDetection.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangeDetection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<GameObject> objectsInRange = new List<GameObject>();
+    void Start()
+    {
+        
+    }
+
+    public List<GameObject> getNearByObjects()
+    {
+        return objectsInRange;
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (!collision.gameObject.CompareTag("Untagged") && collision.gameObject.transform.parent != this.transform.parent)
+        {
+            if (!objectsInRange.Contains(collision.gameObject))
+            {
+                objectsInRange.Add(collision.gameObject);
+                Debug.Log("object:" + collision.gameObject.name);
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        objectsInRange.Remove(other.gameObject);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("clearing");
+    }
+}
