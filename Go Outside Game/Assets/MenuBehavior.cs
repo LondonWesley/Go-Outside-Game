5f598bbf9c38bfce7b9c6ca4fc7f684d7cedbf2b
Jeffrey Hernandez <[jjhernandezvillarreal@aggies.ncat.edu]>
2019-11-23 18:05:40 -0500

    Main Menu is up! Start Game is complete! Will add credits and a game over screen.

---
+++ Go Outside Game/Assets/MenuBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuBehavior : MonoBehaviour
+{
+    public bool isStart;
+    public bool isQuit;
+    public bool isCredits;
+    public Button interaction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        interaction.GetComponent<Button>();
+        interaction.onClick.AddListener(ClickedAction);
+    }
+
+    // Update is called once per frame
+    void ClickedAction()
+    {
+        if(isStart == true)
+        {
+            SceneManager.LoadScene(1);
+        } else if(isQuit == true)
+        {
+            Application.Quit();
+        } else if(isCredits == true)
+        {
+            
+        }
+    }
+}
