a92f691225f5e988e96da77daa57a3c6a8ba0373
Jeffrey Hernandez <[jjhernandezvillarreal@aggies.ncat.edu]>
2019-11-23 20:56:06 -0500

    We now have a credits screen and a game over screen! Gonna try to make the game over screen pop out when your character dies. Game over screen also comes with quotes! So, if you wanna add some more, go to GameOverQuoting script

---
+++ Go Outside Game/Assets/GameOverQuoting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverQuoting : MonoBehaviour
+{
+    public Text motivation = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int rand = Random.Range(0, 5);
+        string[] tArray = {
+        "Don't give up! You have people waiting for you and a home to return to. Don't abandon them...",
+        "Is that all the strength you have? Get up, Ranger! You can do better.",
+        "What an embrassing way to die... Go redeem yourself, Ranger!",
+        "Feel that? That's a wound to your pride. You can either keep that wound or fight back!",
+        "Ranger, you earned this job. Show those things why this park is yours!",
+        "Please... don't give those things the satisfication of victory. Don't let them beat your humanity!"
+        };
+        motivation.text = tArray[rand];
+    }
+}
---
+++ Go Outside Game/Assets/ToLiveOrDie.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ToLiveOrDie : MonoBehaviour
+{
+    public bool MarchOn;
+    public bool Return;
+    public Button choice;
+    // Start is called before the first frame update
+    void Start()
+    {
+        choice.GetComponent<Button>();
+        choice.onClick.AddListener(Decision);
+    }
+
+    // Update is called once per frame
+    void Decision()
+    {
+        if(MarchOn == true)
+        {
+            SceneManager.LoadScene(1);
+        } else if(Return == true)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
